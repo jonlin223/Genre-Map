5c5e3f6369459a2ac67f041c60b4a5e4363089e4
Jonathan Lin <jonlin223@gmail.com>
2024-09-04 16:40:11 +1000

    Update analysis algorithm to consider proportions

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,7 +1,8 @@
+from collections import Counter
 import itertools
 import json
 import os
-from typing import List
+from typing import List, Dict
 
 from my_types import AlbumGenre
 import utils
@@ -18,6 +19,40 @@ import utils
 # Output format
     # {(genre_id_1, genre_id_2): weight} -> List of {source: id1, target: id2, id: 'id1-id2', size} -> EDGES
     # {id: label} -> List of {id, label}
+
+# A potential algorithm
+    # Number of albums where both genres appear
+        # we already have a form of this that takes subgenre into account
+    # Number of albums that each genre appears in
+    # Number of albums total
+    # algo : weight = (A and B) / (A or B)
+        # A or B = A + B - (A and B)
+    # TODO consider ISSUE
+        # Weight scales off popularity
+        # e.g. Even if genre B is very similar to genre A, if Genre B is very unpopular compared to A, weight will be lowered
+
+MAIN_GENRE_VAL = 1
+SUB_GENRE_VAL = 0.5
+
+def get_album_counts(data_src: str) -> Dict[int, int]:
+
+    if not os.path.exists(data_src):
+        raise Exception(f"Path {data_src} does not exist.")
+    
+    with open(data_src) as f:
+        album_genres: List[AlbumGenre] = json.load(f)
+    
+    main_count = Counter()
+    sub_count = Counter()
+    for album_genre in album_genres:
+        main_count.update(album_genre['main_genres'])
+        sub_count.update(album_genre['sub_genres'])
+    
+    counts = dict(main_count)
+    for (id, num) in sub_count.items():
+        counts[id] = counts.get(id, 0) + (SUB_GENRE_VAL * num)
+    
+    return counts
 
 def get_nodes(data_src: str):
     nodes = {}
@@ -67,7 +102,12 @@ def get_edges(data_src: str):
         for key in sub_sub:
             edges[key] = edges.get(key, 0) + 0.25 """
     
-    return [{'source': str(id1), 'target': str(id2), 'id': f"{id1}-{id2}", 'size': weight} for ((id1, id2), weight) in edges.items()]
+    album_counts = get_album_counts(data_src)
+
+    def calc_weight(num_a: int, num_b: int, num_ab: int):
+        return  10 * num_ab / (num_a + num_b - num_ab)
+
+    return [{'source': str(id1), 'target': str(id2), 'id': f"{id1}-{id2}", 'size': calc_weight(album_counts[id1], album_counts[id2], weight)} for ((id1, id2), weight) in edges.items()]
 
 
 if __name__ == "__main__":
